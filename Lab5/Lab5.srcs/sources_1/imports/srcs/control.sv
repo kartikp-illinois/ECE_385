981219d280a4dbb4e5c50d72aa46d0aa71e55cd0
Kyle Stadler <kstadler@Kyles-MacBook-Pro-205.local>
2025-10-09 14:01:55 -0500

    pusing KCS control and cpu.sv - not working but just progress

--- Lab5/Lab5.srcs/sources_1/imports/srcs/control.sv
+++ Lab5/Lab5.srcs/sources_1/imports/srcs/control.sv
@@ -28,7 +28,8 @@ module control (
 	input logic			reset,
 
 	input logic  [15:0]	ir,
-	input logic			ben,
+	//input logic			ben,
+	input logic	[2:0]	nzp,
 
 	input logic 		continue_i,
 	input logic 		run_i,
@@ -37,28 +38,69 @@ module control (
 	output logic		ld_mdr,
 	output logic		ld_ir,
 	output logic		ld_pc,
+	output logic 		ld_reg,
+	output logic 		ld_cc,
 	output logic        ld_led,
 						
-	output logic		gate_pc,
+	output logic		gate_pc,	//treat these as 1-hot 
 	output logic		gate_mdr,
+	output logic 		gate_marmux,
+	output logic		gate_alu,
 						
 	output logic [1:0]	pcmux,
+	output logic 		drmux,
+	output logic 		sr1mux,
+	output logic		sr2mux,
+	output logic [1:0]	aluk,
+	output logic 		addr1mux,
+	output logic [1:0]	addr2mux,
+	output logic 		mioen,
 	
 	//You should add additional control signals according to the SLC-3 datapath design
-
 	output logic		mem_mem_ena, // Mem Operation Enable
 	output logic		mem_wr_ena  // Mem Write Enable
 );
-	enum logic [4:0] {
+	enum logic [5:0] {
 		halted, 
 		pause_ir1,
 		pause_ir2, 
-		s_18, 
-		s_33_1,
-		s_33_2,
-		s_33_3,
-		s_35
-	} state, state_nxt;   // Internal state logic
+
+		s_18,	//fetch1
+		s_33_1,	//fetch2
+		s_33_2,	//fetch3
+		s_33_3,	//fetch4
+		s_35,	//fetch5
+		s_32,	//decode
+
+		s_01,	//add
+
+		s_05,	//and
+
+		s_09,	//not
+
+		s_00,	//br
+		s_22,
+
+		s_12,	//jmp
+
+		s_04,	//jsr
+		s_21,
+
+		s_06_1,	//ldr
+		s_06_2,
+		s_06_3,
+		s_06_4,
+		s_06_5,
+		s_25,
+		s_27,
+
+
+		s_07,	//str
+		s_23,
+		s_16
+	} state, state_nxt;
+
+
 
 
 	always_ff @ (posedge clk)
@@ -68,29 +110,39 @@ module control (
 		else 
 			state <= state_nxt;
 	end
-   
+
 	always_comb
 	begin 
 		
 		// Default controls signal values so we don't have to set each signal
 		// in each state case below (If we don't set all signals in each state,
 		// we can create an inferred latch)
+		// Default control signal values
 		ld_mar = 1'b0;
 		ld_mdr = 1'b0;
 		ld_ir = 1'b0;
 		ld_pc = 1'b0;
+		ld_reg = 1'b0;
+		ld_cc = 1'b0;
 		ld_led = 1'b0;
-		
+
 		gate_pc = 1'b0;
 		gate_mdr = 1'b0;
-		 
+		gate_marmux = 1'b0;
+		gate_alu = 1'b0;
+
 		pcmux = 2'b00;
-		
+		drmux = 1'b0;
+		sr1mux = 1'b0;
+		sr2mux = 1'b0;
+		aluk = 2'b00;
+		addr1mux = 1'b0;
+		addr2mux = 2'b00;
+		mioen = 1'b0;
+
 		mem_mem_ena = 1'b0;
-        mem_wr_ena = 1'b0;
+		mem_wr_ena = 1'b0;
   
-		
-	
 		// Assign relevant control signals based on current state
 		case (state)
 			halted: ; 
@@ -105,20 +157,139 @@ module control (
 				begin
 					mem_mem_ena = 1'b1;
 					ld_mdr = 1'b1;
+					mioen = 1'b1;
+
 				end
 			s_35 : 
 				begin 
 					gate_mdr = 1'b1;
 					ld_ir = 1'b1;
 				end
-			pause_ir1: ld_led = 1'b1; 
-			pause_ir2: ld_led = 1'b1; 
-			// you need to finish the rest of state output logic..... 
+			pause_ir1 : ld_led = 1'b1; 
+			pause_ir2 : ld_led = 1'b1; 
+
+			s_32 : ; //do nothing - its decode, handled in state transition logic below
+
+			s_01 : begin
+					ld_reg = 1'b1;
+					drmux = 1'b0;
+					sr1mux = 1'b1;
+					sr2mux = ir[5];
+					aluk = 2'b00;//ADD;
+					gate_alu = 1'b1;
+					ld_cc = 1'b1;
+				end
+			
+			s_05 : begin
+					ld_reg = 1'b1;
+					drmux = 1'b0;
+					sr1mux = 1'b1;
+					sr2mux = ir[5];
+					aluk = 2'b01;//AND;
+					gate_alu = 1'b1;
+					ld_cc = 1'b1;
+			end
+
+			s_09 : begin
+					ld_reg = 1'b1;
+					drmux = 1'b0;
+					sr1mux = 1'b1;
+					aluk = 2'b10;//NOT;
+					gate_alu = 1'b1;
+					ld_cc = 1'b1;
+			end
+
+			s_06_1 : begin
+					ld_mar = 1'b1;
+					addr2mux = 2'b10;
+					sr1mux = 1'b1;
+					gate_marmux = 1'b1;
+			end
+
+			s_06_2 : begin
+					mem_mem_ena = 1'b1;
+			end
+
+			s_06_3 : begin
+					mem_mem_ena = 1'b1;
+			end
+
+			s_06_4 : begin
+					mem_mem_ena = 1'b1;
+			end
+
+			s_06_5 : begin
+					mem_mem_ena = 1'b1;
+			end
+
+			s_25 : begin
+					ld_mdr = 1'b1;
+					mioen = 1'b1;
+			end
+
+			s_27 : begin
+					ld_reg = 1'b1;
+					gate_mdr = 1'b1;
+					ld_cc = 1'b1;
+			end
+
+			s_07 : begin
+					ld_mar = 1'b1;
+					addr2mux = 2'b10;
+					sr1mux = 1'b1;
+					gate_marmux = 1'b1;
+			end
+
+			s_23 : begin
+					ld_mdr = 1'b1;
+					sr1mux = 1'b1;
+					aluk = 2'b11;//PASSA;
+					gate_alu = 1'b1;
+			end
+
+			s_16 : begin
+					mem_mem_ena = 1'b1;
+					mem_wr_ena = 1'b1;
+			end
+
+			s_00 : begin
+					//... do nothing?
+			end
+
+			s_22 : begin
+					ld_pc = 1'b1;
+					pcmux = 2'b01;
+					addr1mux = 1'b0;
+					addr2mux = 2'b01;
+			end
+
+			s_12 : begin
+					ld_pc = 1'b1;
+					sr1mux = 1'b1;
+					sr2mux = 2'b11;
+					pcmux = 2'b01;
+			end
+
+			s_04 : begin
+					drmux = 1'b1;
+					ld_reg = 1'b1;
+					gate_pc = 1'b1;
+			end
+
+			s_21 : begin
+					ld_pc = 1'b1;
+					addr1mux = 1'b0;
+					addr2mux = 2'b00;
+					pcmux = 2'b01;
+			end
 
 			default : ;
 		endcase
 	end 
 
+
+	logic [3:0] opcode;
+	assign opcode = ir[15:12];
 
 	always_comb
 	begin
@@ -138,18 +309,49 @@ module control (
 			s_33_3 : 
 				state_nxt = s_35;
 			s_35 : 
-				state_nxt = pause_ir1;
-			// pause_ir1 and pause_ir2 are only for week 1 such that TAs can see 
-			// the values in ir.
-			pause_ir1 : 
-				if (continue_i) 
-					state_nxt = pause_ir2;
-			pause_ir2 : 
-				if (~continue_i)
-					state_nxt = s_18;
-			// you need to finish the rest of state transition logic.....
-			
-			default :;
+				state_nxt = s_32;
+			s_32 :
+				begin
+					case(opcode)
+						4'b0001 : state_nxt = s_01;//add
+						4'b0101 : state_nxt = s_05;//and
+						4'b1001 : state_nxt = s_09;//not
+						4'b0000 : state_nxt = s_00;//br
+						4'b1100 : state_nxt = s_12;//jmp
+						4'b0100 : state_nxt = s_04;//jsr
+						4'b0110 : state_nxt = s_06_1;//ldr
+						4'b0111 : state_nxt = s_07;//str
+						4'b1101 : state_nxt = pause_ir1;//pause
+						default : state_nxt = pause_ir1;//unknown
+					endcase
+				end
+			s_01 : state_nxt = s_18;
+			s_05 : state_nxt = s_18;
+			s_09 : state_nxt = s_18;
+			s_06_1 : state_nxt = s_06_2;
+			s_06_2 : state_nxt = s_06_3;
+			s_06_3 : state_nxt = s_06_4;
+			s_06_4 : state_nxt = s_06_5;
+			s_06_5 : state_nxt = s_25;
+			s_25 : state_nxt = s_27;
+			s_07 : state_nxt = s_23;
+			s_23 : state_nxt = s_16;
+			s_16 : state_nxt = s_18;
+
+			s_04 : state_nxt = s_21;
+			s_21 : state_nxt = s_18;
+
+			s_12 : state_nxt = s_18;
+
+			s_00 : state_nxt = (nzp & ir[11:9]) ? s_22 : s_18;
+
+			s_22 : state_nxt = s_18;
+			s_27 : state_nxt = s_18;
+
+			pause_ir1 : state_nxt = continue_i ? pause_ir2 : pause_ir1;
+			pause_ir2 : state_nxt = continue_i ? pause_ir2 : s_18;	
+
+			default : state_nxt = pause_ir1;
 		endcase
 	end
 	
--- Lab5/Lab5.srcs/sources_1/imports/srcs/cpu.sv
+++ Lab5/Lab5.srcs/sources_1/imports/srcs/cpu.sv
@@ -38,90 +38,500 @@ module cpu (
 
 
 // Internal connections, follow the datapath block diagram and add the additional needed signals
-logic ld_mar; 
-logic ld_mdr; 
-logic ld_ir; 
-logic ld_pc; 
-logic ld_led;
+logic		ld_mar;
+logic		ld_mdr;
+logic		ld_ir;
+logic		ld_pc;
+logic		ld_reg;
+logic		ld_cc;
+logic		ld_led;
 
-logic gate_pc;
-logic gate_mdr;
+logic		gate_pc;
+logic		gate_mdr;
+logic		gate_marmux;
+logic		gate_alu;
 
-logic [1:0] pcmux;
-
-logic [15:0] mar; 
-logic [15:0] mdr;
-logic [15:0] ir;
-logic [15:0] pc;
-
-logic [15:0] pc_plus1;
-logic [15:0] pc_next;
-
-logic ben;
+logic [1:0]	pcmux;
+logic		drmux;
+logic		sr1mux;
+logic		sr2mux;
+logic [1:0]	aluk;
+logic		addr1mux;
+logic [1:0]	addr2mux;
+logic		mioen;
 
 
-assign mem_addr = mar;
-assign mem_wdata = mdr;
-assign pc_plus1 = pc + 16'h0001;
+logic           multiDriveErr;
+logic [15:0]    bus;
+logic [15:0]    pc_d;
+logic [15:0]    pcmux_d;
+logic [15:0]    pcinc_d;
+logic [15:0]    marmux_d;
+logic [2:0]     drmux_d;
+logic [2:0]     sr2addr_d; // = irsmth
+logic [2:0]     sr1addr_d;
+logic [15:0]    sr2_d;
+logic [15:0]    sr1_d;
+logic [15:0]    sr2mux_d;
+logic [15:0]    alu_d;
+logic [2:0]     nzp_d;
+//logic [11:0]    imms_d;
+logic [15:0]    sext11_d;
+logic [15:0]    sext9_d;
+logic [15:0]    sext6_d;
+logic [15:0]    sext5_d;
+logic [15:0]    addr2mux_d;
+logic [15:0]    addr1mux_d;
+logic [15:0]    ir_d;
+logic [15:0]    mdr_d;
+logic [15:0]    mar_d;
+logic [15:0]    miomux_d;
+
+
+assign mem_addr = mar_d;
+assign mem_wdata = mdr_d;
+assign sr2addr_d = ir_d[2:0];
 
 // State machine, you need to fill in the code here as well
 // .* auto-infers module input/output connections which have the same name
 // This can help visually condense modules with large instantiations, 
 // but can also lead to confusing code if used too commonly
-control cpu_control (
-    .*
+// control cpu_control (
+//     .*
+// );
+
+control control_inst (
+	.clk(clk),
+	.reset(reset),
+	
+	.ir(ir_d),
+	//.ben(ben),
+    .nzp(nzp_d),
+	
+	.continue_i(continue_i),
+	.run_i(run_i),
+	
+	.ld_mar(ld_mar),
+	.ld_mdr(ld_mdr),
+	.ld_ir(ld_ir),
+	.ld_pc(ld_pc),
+	.ld_reg(ld_reg),
+	.ld_cc(ld_cc),
+	.ld_led(ld_led),
+	
+	.gate_pc(gate_pc),
+	.gate_mdr(gate_mdr),
+	.gate_marmux(gate_marmux),
+	.gate_alu(gate_alu),
+	
+	.pcmux(pcmux),
+	.drmux(drmux),
+	.sr1mux(sr1mux),
+	.sr2mux(sr2mux),
+	.aluk(aluk),
+	.addr1mux(addr1mux),
+	.addr2mux(addr2mux),
+	.mioen(mioen),
+	
+	.mem_mem_ena(mem_mem_ena),
+	.mem_wr_ena(mem_wr_ena)
+);
+/////////////
+assign led_o = ir_d;
+assign hex_display_debug = ir_d;
+////////////////
+
+//bus instantiation
+
+bus bus_inst (
+	.gate_marmux(gate_marmux),
+	.marmux(marmux_d),
+	.gate_pc(gate_pc),
+	.pc(pc_d),
+	.gate_alu(gate_alu),
+	.alu(alu_d),
+	.gate_mdr(gate_mdr),
+	.mdr(mdr_d),
+	.data(bus),
+	.multiDriveErr(multiDriveErr)
 );
 
+//reg instantiaitons 
+load_reg #(.DATA_WIDTH(16)) led_reg (
+    .clk(clk),
+    .reset(reset),
 
-assign led_o = ir;
-assign hex_display_debug = ir;
+    .load(ld_led),
+    .data_i(ir_d), 
 
-load_reg #(.DATA_WIDTH(16)) ir_reg (
-    .clk    (clk),
-    .reset  (reset),
-
-    .load   (ld_ir),
-    .data_i (mdr),
-
-    .data_q (ir)
+    .data_q(led_o)
 );
 
-always_comb begin
-    case (pcmux)
-        2'b00:   pc_next = pc_plus1;
-        default: pc_next = pc_plus1; 
-    endcase
-end
 
 load_reg #(.DATA_WIDTH(16)) pc_reg (
     .clk(clk),
     .reset(reset),
 
     .load(ld_pc),
-    .data_i(pc_next),
+    .data_i(pcmux_d), //should be output of pcmux
 
-    .data_q(pc)
+    .data_q(pc_d)
+);
+
+load_reg #(.DATA_WIDTH(16)) ir_reg (
+    .clk    (clk),
+    .reset  (reset),
+
+    .load   (ld_ir),
+    .data_i (bus),
+
+    .data_q (ir_d)
 );
 
 load_reg #(.DATA_WIDTH(16)) mar_reg (
     .clk    (clk),
     .reset  (reset),
     .load   (ld_mar),
-    .data_i (pc),
-    .data_q (mar)
+    .data_i (bus),
+    .data_q (mar_d)
 );
-
 
 load_reg #(.DATA_WIDTH(16)) mdr_reg (
     .clk    (clk),
     .reset  (reset),
     .load   (ld_mdr),
-    .data_i (mem_rdata),
-    .data_q (mdr)
+    .data_i (miomux_d), //should be output of mio mux
+    .data_q (mdr_d)
+);
+
+//regfile stuff
+drMux drmux_inst(
+    .ir11_9(ir_d[11:9]),
+    .sel(drmux),
+    .dr(drmux_d)
+);
+sr1Mux sr1Mux_inst (
+	.ir11_9(ir_d[11:9]),
+	.ir8_6(ir_d[8:6]),
+	.sel(sr1mux),
+	.dout(sr1addr_d)
+);
+regFile regFile_inst (
+	.clk(clk),
+	.rst(reset),
+	.ld_reg(ld_reg),
+	.dr(drmux_d),
+	.din(bus),
+	.sr1_addr(sr1addr_d),
+	.sr2_addr(sr2addr_d),
+	.sr1(sr1_d),
+	.sr2(sr2_d)
+);
+sr2DMux sr2DMux_inst (
+	.sr2(sr2_d),
+	.imm(sext5_d),
+	.sel(sr2mux),
+	.dout(sr2mux_d)
 );
 
 
+//alu
+alu alu_inst (
+	.a(sr1_d),
+	.b(sr2mux_d),
+	.aluOp(aluk),
+	.dout(alu_d)
+);
+
+//cc nzp stuff
+controlCodes controlCodes_inst (
+	.clk(clk),
+	.rst(reset),
+	.ld(ld_cc),
+	.bus(bus),
+	.nzp(nzp_d)
+);
+
+
+//sext stuff
+sext #(.WIDTH(11)) sext11_inst (
+	.din(ir_d[10:0]),
+	.dout(sext11_d)
+);
+
+sext #(.WIDTH(9)) sext9_inst (
+	.din(ir_d[8:0]),
+	.dout(sext9_d)
+);
+
+sext #(.WIDTH(6)) sext6_inst (
+	.din(ir_d[5:0]),
+	.dout(sext6_d)
+);
+
+sext #(.WIDTH(5)) sext5_inst (
+	.din(ir_d[4:0]),
+	.dout(sext5_d)
+);
+
+
+//muxs to pcadder
+addr2Mux addr2Mux_inst (
+	.sext11(sext11_d),
+	.sext9(sext9_d),
+	.sext6(sext6_d),
+	.sel(addr2mux),
+	.dout(addr2mux_d)
+);
+
+addr1Mux addr1Mux_inst (
+	.fromPC(pc_d),
+	.fromSr1(sr1_d),
+	.sel(addr1mux),
+	.dout(addr1mux_d)
+);
+
+adder adder_inst (
+	.a(addr2mux_d),
+	.b(addr1mux_d),
+	.dout(marmux_d)
+);
+
+pcmux pcmux_inst (
+	.fromBus(bus),
+	.fromAdder(marmux_d),
+	.fromPcInc(pcinc_d),
+	.sel(pcmux),
+	.dout(pcmux_d)
+);
+
+pcInc pcInc_inst (
+	.pcIn(pc_d),
+	.pcOut(pcinc_d)
+);
+
+
+//mmio stuff
+mioMux mioMux_inst (
+	.fromBus(bus),
+	.rdata(mem_rdata),
+	.sel(mioen),
+	.dout(miomux_d)
+);
+
+// //MMIO stuff DID THIS AT THE TOP OF THE FILE ALREADY
+// // mem_wdata,
+// //mem_addr,
+// //mem_mem_ena, //handled from control
+// //mem_wr_ena    //handled from control 
+// always_comb begin
+//     mem_wdata = mdr_d;
+//     mem_addr = mar_d;
+// end
+
+endmodule
 
 
 
+
+module mioMux(
+    input logic [15:0] fromBus,
+    input logic [15:0] rdata,
+    input logic sel,
+    output logic [15:0] dout
+);
+    assign dout = sel ? rdata : fromBus;
+endmodule
+
+module adder(
+    input logic [15:0] a,
+    input logic [15:0] b,
+    output logic [15:0] dout
+);
+    assign dout = a+b;
+endmodule
+
+module addr1Mux(
+    input logic [15:0] fromPC,
+    input logic [15:0] fromSr1,
+    input logic sel,
+    output logic [15:0] dout
+);
+    assign dout = sel ? fromSr1 : fromPC;
+endmodule
+
+module addr2Mux(
+    input logic [15:0] sext11,
+    input logic [15:0] sext9,
+    input logic [15:0] sext6,
+    //last one is 0
+    input logic [1:0] sel,
+    output logic [15:0] dout
+);
+    always_comb begin
+        case(sel)
+            2'b00 : dout = sext11;
+            2'b01 : dout = sext9;
+            2'b10 : dout = sext6;
+            2'b11 : dout = 16'h0000;
+        endcase
+    end
+endmodule
+
+
+module sext #(
+    parameter WIDTH = 6//defualt widht
+)(
+    input logic [WIDTH-1:0] din,
+    output logic [15:0] dout
+);
+    always_comb begin
+        dout = {{(16-WIDTH){din[WIDTH-1]}}, din};
+    end
+endmodule
+
+module controlCodes(
+    input logic clk,
+    input logic rst,
+    input logic ld,
+    input logic [15:0] bus,
+    output reg [2:0] nzp
+);
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            nzp <= 3'b000;
+        end else begin
+            if (ld) begin
+                nzp <= {bus[15], bus==16'd0, !bus[15]};
+            end
+        end
+    end
+endmodule
+
+module alu(
+    input logic [15:0] a,
+    input logic [15:0] b,
+    input logic [1:0] aluOp,
+    output logic [15:0] dout
+);
+    always_comb begin
+        case(aluOp)
+            2'b00 : dout = a+b;//add
+            2'b01 : dout = a&b;//and
+            2'b10 : dout = ~a;//not
+            2'b11 : dout = a;//passA
+            default : dout = 16'hDED2;
+        endcase
+    end
+endmodule
+
+module sr2DMux(
+    input logic [15:0] sr2,
+    input logic [15:0] imm,
+    input logic sel,
+    output logic [15:0] dout
+);
+    assign dout = sel ? imm : sr2;
+endmodule
+
+module sr1Mux(
+    input logic [2:0] ir11_9,
+    input logic [2:0] ir8_6,
+    input logic sel,
+    output logic [2:0] dout
+);
+    assign dout = sel ? ir8_6 : ir11_9;
+endmodule
+
+module regFile(
+    input logic clk,
+    input logic rst,
+    input logic ld_reg,
+    input logic [2:0] dr,
+    input logic [15:0] din,
+    input logic [2:0] sr1_addr,
+    input logic [2:0] sr2_addr,
+    output logic [15:0] sr1,
+    output logic [15:0] sr2
+);
+    logic [15:0] rf [8];
+    logic rst_val = 16'hxxxx;
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            rf[0] <= rst_val;
+            rf[1] <= rst_val;
+            rf[2] <= rst_val;
+            rf[3] <= rst_val;
+            rf[4] <= rst_val;
+            rf[5] <= rst_val;
+            rf[6] <= rst_val;
+            rf[7] <= rst_val;
+        end else begin
+            if (ld_reg) begin
+                rf[dr] <= din;
+            end
+        end
+    end
+    always_comb begin
+        sr1 = rf[sr1_addr];
+        sr2 = rf[sr2_addr];
+    end
+endmodule
+
+module drMux(
+    input logic [2:0] ir11_9,
+    input logic sel,
+    output logic [2:0] dr
+);
+    assign dr = sel ? 3'b111 : ir11_9;
+endmodule
+
+module pcInc(
+    input logic [15:0] pcIn,
+    output logic [15:0] pcOut
+);
+    assign pcOut = pcIn + 16'd1;
+endmodule
+
+module pcmux(
+    input logic [15:0] fromBus,
+    input logic [15:0] fromAdder,
+    input logic [15:0] fromPcInc,
+    input logic [1:0] sel,
+    output logic [15:0] dout
+);
+    always_comb begin
+        case(sel)
+            2'b00 : dout = fromBus;
+            2'b01 : dout = fromAdder;
+            2'b10 : dout = fromPcInc;
+            default : dout = 16'hDED1;
+        endcase
+    end
+endmodule
+
+module bus(
+    input logic gate_marmux,
+    input logic [15:0] marmux,
+    input logic gate_pc,
+    input logic [15:0] pc,
+    input logic gate_alu,
+    input logic [15:0] alu,
+    input logic gate_mdr,
+    input logic [15:0] mdr,
+    output logic [15:0] data,
+    output logic multiDriveErr
+);  
+    logic [1:0] ctr;
+    assign multiDriveErr = (ctr > 1);
+    always_comb begin
+        ctr = gate_marmux + gate_pc + gate_alu + gate_mdr;
+        case({gate_marmux, gate_pc, gate_alu, gate_mdr})
+            4'b1000 : data = marmux;
+            4'b0100 : data = pc;
+            4'b0010 : data = alu;
+            4'b0001 : data = mdr;
+            default : data = 16'hDED0;
+        endcase
+    end
 endmodule
